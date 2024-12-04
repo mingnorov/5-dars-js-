37bead860d1b5989da065b0dbf4cb9dd7cede271
mingnorov <zovlonmingnorov@gmail.com>
2024-12-04 12:02:22 +0300

    Add files via upload

---
+++ 5 dars/script.js
@@ -0,0 +1,11 @@
+const name = prompt("Ismingizni kiriting:");
+const spaceId = prompt("Space ID-ni kiriting:");
+const spacePassword = prompt("Space parolini kiriting:");
+
+const userInfo = {
+  name: name,
+  id: spaceId,
+  password: spacePassword,
+};
+
+console.log(userInfo);
